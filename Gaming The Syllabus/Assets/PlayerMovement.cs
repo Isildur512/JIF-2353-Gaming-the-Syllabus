7a651072479645359e635bbcec62ae4eb7ebeee9
Isildur512 <78391349+Isildur512@users.noreply.github.com>
2022-11-25 14:22:27 -0500

    Implemented Basic Movement

---
+++ Gaming The Syllabus/Assets/PlayerMovement.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public Vector2 speed = new Vector2(50,50);
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float xInput = Input.GetAxis("Horizontal");
+        float yInput = Input.GetAxis("Vertical");
+        
+        Vector3 movement = new Vector3(speed.x * xInput, speed.y * yInput, 0);
+
+        movement *= Time.deltaTime;
+
+        transform.Translate(movement);
+    }
+}
