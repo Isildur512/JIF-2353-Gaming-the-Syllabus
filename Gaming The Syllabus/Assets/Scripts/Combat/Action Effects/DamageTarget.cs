4292fd098f7964a86694b5f250f8c3d8edbe0dc9
travis-bittel <tbittel6@gatech.edu>
2023-01-25 15:49:11 -0500

    Add XML customizable enemies

---
+++ Gaming The Syllabus/Assets/Scripts/Combat/Action Effects/DamageTarget.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Xml.Serialization;
+using System.Xml.Schema;
+using System.Xml;
+
+public class DamageTarget : ActionEffect
+{
+    private int damageAmount;
+
+    public DamageTarget(int damageAmount, TargetType target)
+    {
+        this.damageAmount = damageAmount;
+        _target = target;
+    }
+
+    public DamageTarget()
+    {
+
+    }
+
+    public override void Apply(params CombatUnit[] targets)
+    {
+        foreach (CombatUnit target in targets)
+        {
+            Debug.Log($"Dealing {damageAmount} damage to {target}");
+            target.ApplyDamage(damageAmount);
+        }
+    }
+
+    public override void WriteXml(XmlWriter writer)
+    {
+        base.WriteXml(writer);
+        writer.WriteAttributeString("damageAmount", damageAmount.ToString());
+    }
+
+    public override void ReadXml(XmlReader reader)
+    {
+        base.ReadXml(reader);
+        damageAmount = int.Parse(reader.GetAttribute("damageAmount"));
+    }
+}
---
+++ Gaming The Syllabus/Assets/Scripts/Combat/CombatUnit.cs
@@ -0,0 +1,73 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Xml.Serialization;
+using System.Xml.Schema;
+using System.Xml;
+
+/// <summary>
+/// A unit participating in combat. Used by <see cref="CombatManager"/>.
+/// </summary>
+[XmlRoot("CombatUnit")]
+public class CombatUnit : IXmlSerializable
+{
+    [XmlAttribute("name")]
+    public string unitName;
+
+    public int maximumHealth;
+    public int currentHealth;
+
+    public List<UnitAction> actions;
+
+    public void PerformTurn()
+    {
+        actions[0].Execute();
+    }
+
+    public void ApplyDamage(int amount)
+    {
+        currentHealth -= amount;
+    }
+
+    public XmlSchema GetSchema()
+    {
+        return (null);
+    }
+
+    public void ReadXml(XmlReader reader)
+    {
+        unitName = reader.GetAttribute("name");
+        maximumHealth = int.Parse(reader.GetAttribute("maximumHealth"));
+        currentHealth = int.Parse(reader.GetAttribute("currentHealth"));
+
+        Debug.Log($"Name: {unitName}, Max HP: {maximumHealth}, Current: {currentHealth}");
+
+        // TODO: Make this work for any number of actions, not just 1
+        actions = new List<UnitAction>();
+        reader.ReadToDescendant("actions");
+
+        UnitAction action = new UnitAction();
+        action.ReadXml(reader);
+        actions.Add(action);
+    }
+
+    public void WriteXml(XmlWriter writer)
+    {
+        writer.WriteAttributeString("xmlns:xsd", "http://www.w3.org/2001/XMLSchema");
+        writer.WriteAttributeString("xmlns:xsi", "http://www.w3.org/2001/XMLSchema-instance");
+        writer.WriteAttributeString("name", unitName);
+        writer.WriteAttributeString("maximumHealth", maximumHealth.ToString());
+        writer.WriteAttributeString("currentHealth", currentHealth.ToString());
+
+        writer.WriteStartElement("actions");
+
+        foreach (UnitAction action in actions)
+        {
+            writer.WriteStartElement("action");
+            action.WriteXml(writer);
+            writer.WriteEndElement();
+        }
+
+        writer.WriteEndElement();
+    }
+}
